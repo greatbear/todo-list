b43629a915e9d23314a0d93f1c7f8fe658cebb12
greatbear <greatbear@kraken.vn>
2010-02-02 00:55:06 +0700

    add + del func

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,10 +3,21 @@ class TodoController < ApplicationController
   def index
     @tasks = Task.list_tasks
   end
-  def list
+  def edit
   end
   # add new item
   def add
   end
-
+  def addItem
+    item = Task.new
+    item.name = params[:name]
+    item.description = params[:des]
+    item.save
+    redirect_to(:action => '')
+  end
+  def delete
+    id = params[:id]
+    Task.delete(id)
+    redirect_to(:action => '')
+  end
 end
